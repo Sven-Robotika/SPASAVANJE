d12b592b23f965754a9da8a6e1d71281e4a016e2
Sven-Robotika <114673221+Sven-Robotika@users.noreply.github.com>
2022-10-01 09:47:17 +0200

    Update 1_radionica.h

--- radionice-2022/1_radionica.h
+++ radionice-2022/1_radionica.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "helper.h"
-
 void RobotLine::radionica() {
+// ZADATAK: Prikaži naziv robota.
+// HINT: display(); 
+display("SVEN");
 
+// ZADATAK: Neka robot opiše četverokut. 
+go(60,60);
+delayMs(3000);
+go(50,-50);
+delayMs(1000);
 }
